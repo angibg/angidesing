cc245620e200ccf7e5b9758471a9ab646be22ba1
angibg <angibatiz12@gmail.com>
2026-03-18 17:35:47 +0000

    subiendo proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+// MENSAJE AL HACER CLICK
+function mensaje() {
+  alert("Bienvenido a Angi Desing 🚀");
+}
+
+// CAMBIAR TEXTO DINÁMICAMENTE
+function cambiarTexto() {
+  document.getElementById("titulo").innerText = "Bienvenido a Angi Desing";
+}
+
+// EFECTO AL CARGAR LA PAGINA
+window.onload = function() {
+  console.log("Página cargada correctamente");
+}
